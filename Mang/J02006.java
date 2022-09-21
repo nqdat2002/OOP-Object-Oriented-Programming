28b1e3afdddbb3f0b1f89c7ea0d29d1be3e20ad5
Nguyễn Quốc Đạt <84000456+nqdat2002@users.noreply.github.com>
2022-09-21 22:48:35 +0700

    Update J02006.java

--- Mang/J02006.java
+++ Mang/J02006.java
@@ -1,23 +1,16 @@
 package OOPs.Mang;
 
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class J02006 {
-    public static void main(String [] args){
-        Scanner ip = new Scanner(System.in);
-        int n = ip.nextInt(), m = ip.nextInt();
-        int [] a = new int[1005], b = new int[1005];
-        for(int i = 0; i < n; ++i) {
-            int x = ip.nextInt();
-            a[x]++;
-        }
-        for(int i = 0; i < m; ++i) {
-            int x = ip.nextInt();
-            b[x]++;
-        }
-        for(int i = 0; i < 1005; ++i){
-            if(a[i] != 0 && b[i] != 0) System.out.print(i + " ");
-        }
-        ip.close();
+   public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), m = sc.nextInt();
+        Set<Integer> st = new TreeSet<>();
+        for (int i = 0; i < n + m; ++i) st.add(sc.nextInt());
+        for(Integer x: st) System.out.print(x + " ");
+        sc.close();
     }
 }
